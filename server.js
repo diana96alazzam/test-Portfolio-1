1df4c4f771ba6fb366dcf86405e5d6509e96dc78
diana96alazzam <diana-azzam@STDLTUC-L019.localdomain>
2020-04-03 16:27:38 +0300

    added server

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.static('./public'));
+app.get('*', (request, response) =>
+response.status(404).send('404 page not found')
+);
+// app.listen(PORT, () => console.log(`listening to prot ${PORT}`));
